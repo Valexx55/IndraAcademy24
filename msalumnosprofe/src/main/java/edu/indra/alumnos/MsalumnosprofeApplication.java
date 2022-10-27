fef4cc1d02122feb38f817180d1d903cf3975d7a
valexx55 <valexx55@gmail.com>
2022-10-27 09:48:30 +0200

    Eureka cli

--- msalumnosprofe/src/main/java/edu/indra/alumnos/MsalumnosprofeApplication.java
+++ msalumnosprofe/src/main/java/edu/indra/alumnos/MsalumnosprofeApplication.java
@@ -2,9 +2,21 @@ package edu.indra.alumnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //activamos el cliente de eureka
 public class MsalumnosprofeApplication {
+	
+	/**
+	 * PARA CONFIGUAR EL CLIENTE EUREKA
+	 * 
+	 * 1) ADD DEPENDECIA DE EUREKA-CLIENT
+	 * 2) ADD LA ANOTACIÓN @EnableEurekaClient EN EL MAIN DEL MS
+	 * 3) ADD CONFIG PROPERTIES
+	 * 
+	 * 
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsalumnosprofeApplication.class, args);
